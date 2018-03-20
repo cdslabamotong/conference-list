7b7ab1ba3601bed5feb26f8734b3b8ccc29075db
Nicolas Vanhoren <n.vanhoren@evs.com>
2018-03-20 11:55:59 +0100

    Added prefixfree

--- update_bootstrap.sh
+++ update_bootstrap.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 rm -rf node_modules package-lock.json
-npm install bootstrap@4 jquery@3
+npm install bootstrap@4 jquery@3 prefixfree@1
 
 rm -rf _sass/bootstrap
 mkdir -p _sass/bootstrap
@@ -12,4 +12,5 @@ rm -rf assets/javascript/bootstrap
 mkdir -p assets/javascript/bootstrap
 cp node_modules/bootstrap/dist/js/bootstrap.bundle.min.* assets/javascript/bootstrap/
 cp node_modules/jquery/dist/jquery.min.* assets/javascript/bootstrap/
+cp node_modules/prefixfree/prefixfree.min.js assets/javascript/bootstrap/
 touch assets/javascript/bootstrap/__DO_NOT_MODIFY
